55fd20e003b3bdb526665c382ffecb453e7d96d0
LavrykMykola <lavrykmykola@gmail.com>
2024-05-12 19:41:05 +0200

    added comments

--- get_cats_info.py
+++ get_cats_info.py
@@ -3,15 +3,15 @@ import pathlib
 
 def get_cats_info(path):
     try:
-        with open(path, 'r', encoding="utf-8") as file:
-            cats = file.readlines()
-            list_of_cats = []
-            for cat in cats:
-                info_list = cat.split(",")
+        with open(path, 'r', encoding="utf-8") as file: # відкриваємо файл з правильним кодуванням
+            cats = file.readlines() # Зберігаємо кожен рядок у список cats
+            list_of_cats = [] # Створюємо список з інформацією про котів, який будемо повертати
+            for cat in cats: # Для кожного рядка обробляємо інформацію
+                info_list = cat.split(",") # Розділяємо рядок
                 ID = info_list[0].strip()
                 name = info_list[1].strip()
                 age = info_list[2].strip()
-                list_of_cats.append({'id': ID, 'name': name, 'age': age})
+                list_of_cats.append({'id': ID, 'name': name, 'age': age}) # Додаємо до списку словники
             return list_of_cats
 
     except FileNotFoundError:
@@ -20,9 +20,9 @@ def get_cats_info(path):
         return ['Файл очікує три аргументи на кожен рядок. Переконайтеся, що усі рядки розділені комами']
 
 
-current_dir = pathlib.Path(__file__).parent
+current_dir = pathlib.Path(__file__).parent # Знаходимо шлях до поточної папки
 result = get_cats_info(current_dir / "cats_info.txt")
-if len(result) > 1:
+if len(result) > 1: # Якщо все вдалося і повернуло декілька словників
     print(result)
-else:
+else: # Якщо тільки один елемент у списку - це скоріш за все помилка. Виводимо її.
     print(result[0])
--- get_salary.py
+++ get_salary.py
@@ -3,25 +3,25 @@ import pathlib
 
 def total_salary(path):
     try:
-        with open(path, 'r', encoding="utf-8") as file:
-            salaries = file.readlines()
-            total = 0
-            count = 0
+        with open(path, 'r', encoding="utf-8") as file: # Відкриваємо файл з правильним кодуванням
+            salaries = file.readlines() # Зберігаємо кожен рядок у списку
+            total = 0 # загальна сума
+            count = 0 # кількість розробників 
             for salary in salaries:
-                temp_list = salary.split(",")
-                number = temp_list[1].strip()
+                temp_list = salary.split(",") # розділяємо рядок на розробника і його зарплату
+                number = temp_list[1].strip() # Зберігаємо зарплату
                 total += int(number)
                 count += 1
-            return [total, total/count]
+            return [total, total/count] # повертаємо список з загальною та середньою зарплатою
     except FileNotFoundError:
         return ['File not found']
     except IndexError:
         return ['File does not support valid syntax. Make sure to split all the lines with a comma']
 
 
-current_dir = pathlib.Path(__file__).parent
+current_dir = pathlib.Path(__file__).parent # Знаходимо шлях до поточної папки
 result = total_salary(current_dir / "total_salary.txt")
-if len(result) > 1:
+if len(result) > 1: # Якщо в списку декілька елементів, то все вийшло
     print(f"Загальна сума заробітної плати: {result[0]}, Середня заробітна плата: {result[1]}")
-else:
+else: # Якщо 1 елемент, то скоріш за все помилка, виводимо її
     print(result[0])
